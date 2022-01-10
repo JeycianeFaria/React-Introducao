dc6a591decce7bba2c485ef74d6e17cea09bb8d9
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-10 20:34:09 -0300

    Criar componente utilizando props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ComponenteComProps from './components/ComponenteComProps';
 import PrimeiroComponente from './components/PrimeiroComponente';
 import './index.css';
 
 
 ReactDOM.render(
   <div>
-    <PrimeiroComponente/>
+    <ComponenteComProps nome="André" cargo="Instrutor"/>
+    <ComponenteComProps nome="Vini" cargo="Instrutor"/>
+    <ComponenteComProps nome="Maga" cargo="GP"/>
+    <ComponenteComProps nome="Gi" cargo="GP"/>
   </div>,
   document.getElementById('root')
 );
